b300ffc7a5f9598e4d8e014a15348c77ed7cc42b
JDHatten <squall_@fastmail.fm>
2016-01-12 14:14:16 -0700

    addmore, counter

--- update_ui.bat
+++ update_ui.bat
@@ -2,4 +2,6 @@ call pyuic5 -o window.py window.ui
 call pyuic5 -o confirm.py confirm.ui
 call pyuic5 -o preferences.py preferences.ui
 call pyuic5 -o about.py about.ui
+call pyuic5 -o addmore.py addmore.ui
+call pyuic5 -o counter.py counter.ui
 exit
